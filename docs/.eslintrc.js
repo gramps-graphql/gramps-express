56914a12f24660ac9197f6318e7169c5189688fc
Jason Lengstorf <jason@lengstorf.com>
2017-10-15 18:52:01 -0500

    style: fix ESLint issues

--- docs/.eslintrc.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  extends: "airbnb-base",
-  env: {
-    browser: true,
-  },
-};
--- docs/src/js/lib/docs-lightbox.js
+++ docs/src/js/lib/docs-lightbox.js
@@ -5,18 +5,22 @@ const contentArea = document.querySelector('.content');
 const docsLightbox = document.querySelector('.docs-lightbox');
 
 export default () => {
-  contentArea.addEventListener('click', (event) => {
+  contentArea.addEventListener('click', event => {
     if (event.target.classList.contains('figure__image--lightbox')) {
       const imgSrc = event.target.src;
       docsLightbox.querySelector(`.${getClass('image')}`).src = imgSrc;
 
       // The timeout prevents a flicker of the images changing while fading in
-      setTimeout(() => { docsLightbox.classList.remove(`${getClass()}--disabled`); }, 50);
+      setTimeout(() => {
+        docsLightbox.classList.remove(`${getClass()}--disabled`);
+      }, 50);
     }
   });
 
-  docsLightbox.querySelector(`.${getClass()}__close-btn`).addEventListener('click', (event) => {
-    event.preventDefault();
-    docsLightbox.classList.add(`${getClass()}--disabled`);
-  });
+  docsLightbox
+    .querySelector(`.${getClass()}__close-btn`)
+    .addEventListener('click', event => {
+      event.preventDefault();
+      docsLightbox.classList.add(`${getClass()}--disabled`);
+    });
 };
